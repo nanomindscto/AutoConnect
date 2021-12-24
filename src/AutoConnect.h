49d9349e7b2c40f984e08d90e1832ea75cb2738e
googly <googly@nm.com>
2021-12-24 03:08:49 -0800

    add secure server support for AutoConnect

--- src/AutoConnect.h
+++ src/AutoConnect.h
@@ -16,10 +16,11 @@
 #if defined(ARDUINO_ARCH_ESP8266)
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266WebServerSecure.h>
 extern "C" {
 #include <user_interface.h>
 }
-using WebServerClass = ESP8266WebServer;
+using WebServerClass = BearSSL::ESP8266WebServerSecure;
 #elif defined(ARDUINO_ARCH_ESP32)
 #include <WiFi.h>
 #include <WebServer.h>
